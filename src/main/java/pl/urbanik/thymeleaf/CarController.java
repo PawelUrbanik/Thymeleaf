c529e1fdbe86cc6c1066f584b89c82307dff80ec
Pawel Urbanik <pawelurbanik94@gmail.com>
2019-09-01 11:07:42 +0200

    Iterations

--- src/main/java/pl/urbanik/thymeleaf/CarController.java
+++ src/main/java/pl/urbanik/thymeleaf/CarController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class CarController {
@@ -13,7 +15,16 @@ public class CarController {
     public String getCar(Model model)
     {
         Car car = new Car("X5", "BMW");
+        Car car1 = new Car("X5", "Fiat");
+        Car car2 = new Car("X5", "Ferrari");
+        Car car3 = new Car("X5", "Hiunday");
+        Car car4 = new Car("X5", "Skoda");
+
+        List<Car> carList = Arrays.asList(car, car1, car2, car3, car4);
+        model.addAttribute("cars", carList);
         model.addAttribute("car", car );
+
+
         return "car";
     }
 
